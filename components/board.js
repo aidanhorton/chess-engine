aa5ce890c66a253a6d23d2460ba51c93f258726f
Aidan Horton <aidan.horton@virginmedia.com>
2023-07-03 16:55:28 +0100

    Add first iteration of react app w/ navigation

---
+++ components/board.js
@@ -0,0 +1,16 @@
+const getBoard = x => {
+    let boardContent = [];
+    for (let i = 0; i < 64; i++) {
+        boardContent.push(<p>{i}</p>);
+    }
+
+    return boardContent;
+}
+
+export default function Board() {
+    return (
+        <div>
+            {getBoard()}
+        </div>
+    )
+}
