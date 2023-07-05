4f85a0eff99a7d77e6a149b58999d9f5483e906a
Aidan Horton <aidan.horton@virginmedia.com>
2023-07-05 16:27:52 +0100

    Create basic board

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Board from '../components/board';
+
 export default function Page() {
     return (
         <>
             <h1>Home</h1>
+
+            <Board />
         </>
     )
 }
---
+++ components/square.tsx
@@ -0,0 +1,16 @@
+import styles from './square.module.css';
+
+export default function Square({ index }: { index: number }) {
+    const squareStyle = `${styles.square} ${isDarkSquare(index) ? styles.secondarySquare : ""}`;
+    
+    return (
+        <span className={squareStyle}></span>
+    )
+}
+
+function isDarkSquare(index: number) {
+    const row = Math.floor(index / 8);
+    const col = index % 8;
+
+    return (row % 2 === col % 2);
+}
